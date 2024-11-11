656a867e9304a5ce471348794b4b7bf2df6acda9
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2024-11-11 13:17:05 +0530

    Js Class Basics 2

--- index.js
+++ index.js
@@ -1,70 +1,204 @@
-// line below is used to print on the console window
-console.log('Namaste Dunia version4');
+console.log('Chaliye shuru karte hai ');
 
-// let a = true;
+//object create
+// let rectangle = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+//factory function 
+
+// function createRectangle(len, bre) {
+
+//     const rectangle = {
+//         length: len,
+//         breadth:bre ,
+
+//         draw() {
+//             console.log('drawing rectangle');
+//         }
+// //     };
+//     return rectangle;
+// }
+
+// let rectangleObj1 = createRectangle(5, 4);
+// let rectangle2 = createRectangle(2,3);
+// let rectangle3 = createRectangle(7,9);
+
+//Camelcase -> numberOfStudents
+//constructor function -> Pascal Notation -> first letter of every word is Capital -> NumberOfStudents
+//constructor function -> prop/methods -> intialise/Define
+// function Rectangle(len, bre) {
+//     this.length = len;
+//     this.breadth = bre;
+//     this.draw= function() {
+//         console.log('drawing');
+//     }
+// }
+
+// Rectangle.
+
+// //object creation using constrcutor function
+// let rectangleObject = new Rectangle(4,6);
+
+// rectangleObject.color = 'yellow';
+// console.log(rectangleObject);
+
+// delete rectangleObject.color;
+// console.log(rectangleObject);
+
+// let Rectangle1 = new Function(
+//     'length', 'breadth',
+// `this.length = length;
+// this.breadth = breadth;
+// this.draw= function() {
+//     console.log('drawing');
+// }`);
+
+// //object creation using Rectangle1
+// let rect = new Rectangle1(2,3);
+
+// rect.length;
+
+// console.log(rect);
+
+
+// rectangle.length;
+// rectangle.breadth;
+
+// rectangle.draw();
+
+// let rectangle1 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+
+// let rectangle2 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+
+// let a = 10;
+// let b = a;
+
+// a++;
+// console.log(a);
+// console.log(b);
+
+
+// let a = { value: 10};
+// let b = a;
+
+// a.value++;
+
+// console.log(a.value);
+// console.log(b.value);
+
+// let a = 10;
+
+// function inc(a) {
+//     a++;
+// }
+
+// inc(a);
+
 // console.log(a);
 
-// const num = 12;
-// //num = 13;
+// let a = {value:10 };
 
-// let lastName = 3;
-// console.log(lastName);
-
-// lastName = true;
-// console.log(lastName);
-
-// console.log(5 !== 3);
-
-// let age = 27;
-// let status = (age >= 18) ? 'Yes Vote' : 'No Vote';
-
-// console.log(status);
-
-// let marks = 12;
-
-// if(marks >= 90) {
-//     console.log('A');
+// function inc(a) {
+//     a.value++;
 // }
-// else if(marks >= 80) {
-//     console.log('B');
+
+// inc(a);
+
+// console.log(a.value);
+
+let rectangle = {
+    length:2,
+    breadth:4
+};
+
+//for-in loop 
+// for(let key in rectangle ) {
+//     //keys are reflected through key variable 
+//     //values are reflected through rectangle[key]
+//     console.log(key,rectangle[key]);
 // }
-// else if(marks >=70) {
-//     console.log('C');
+
+//for-of loop
+// for(let key of Object.entries(rectangle)) {
+//     console.log(key);
 // }
-// else if(marks >= 60) {
-//     console.log('D');
+
+// if('length' in rectangle) {
+//     console.log('Present');
 // }
 // else {
-//     console.log('E');
-//}
-
-// let num = 1;
-
-// switch(num) {
-//     case 1: console.log('A');
-//     break;
-//     case 2: console.log('B');
-//     break;
-//     case 3: console.log('C');
-//     break;
-//     default: console.log('D');
+//     console.log('Absent');
 // }
 
 
-// for(let i = 1; i<=5; i=i+1) {
-//     console.log("Babbar" + i);
+//object - colone #1
+// let src = {
+//     a:10,
+//     b:20,
+//     c:30
+// };
+
+// let dest = {};
+
+// for(let key in src) {
+//     dest[key] = src[key];
 // }
 
-// let x = 1;
+// console.log(dest);
 
-// while(x <= 5) {
-//     console.log(x);
-//     x++;
-// }
+// src.a++;
+// console.log(dest);
 
-let y = 1;
+//Object Cloning #2
+// let src = {
+//     a:10,
+//     b:20,
+//     c:30
+// };
 
-do {
-    console.log(y);
-    y++;
-} while(y < 10);
+// let src2 = { value: 25};
+
+// let dest = Object.assign({}, src, src2);
+
+// console.log(dest);
+
+// src.a++;
+
+// console.log(dest);
+
+//Object Cloning #3
+// let src = {
+//     a: 10,
+//     b:20,
+//     c:30
+// };
+
+// let dest = {...src};
+
+// console.log(dest);
+
+// src.a++;
+
+// console.log(dest);
